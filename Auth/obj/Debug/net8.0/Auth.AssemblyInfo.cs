2021ac3bc0b27b85106f223a6a4deb42a7a0db11
Nazar <swimming1999@mail.ru>
2026-07-10 15:04:14 +0300

    gitignore

--- Auth/obj/Debug/net8.0/Auth.AssemblyInfo.cs
+++ Auth/obj/Debug/net8.0/Auth.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578bd4c395a0bd6ce4072c74eb8a168f34438416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1490e8f2dfc1f39a68606c30271d285f64ad7171")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
